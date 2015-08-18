0cbc28a619786e856f39a82e94a6c109ff54fc1f
Andrew 'Doc' Docherty <doc@coveycs.com>
2015-08-18 00:00:04 -0400

    fixed windows symlink issue

--- install-run.sh
+++ install-run.sh
@@ -5,9 +5,9 @@
     # Symlink our source to the host's dir
     #ln -s ~/react-fullstack/ /vagrant/project   
 
-	mv /home/vagrant/react-fullstack/ /vagrant/react-fullstack/
+	#mv /home/vagrant/react-fullstack/ /vagrant/react-fullstack/
    
 	# Victory!
 	echo "You're almost done! Your default node server should now be set up to run on the host machine at localhost:3000."
-	cd /vagrant/react-fullstack
+	#cd /vagrant/react-fullstack
     gulp
